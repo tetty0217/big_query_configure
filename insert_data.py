d27474baf9471c770e5ee302034f12d1eb21e8dd
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2019-01-23 17:58:56 +0900

    fixed test json

--- insert_data.py
+++ insert_data.py
@@ -6,11 +6,11 @@ from gbq_setting import DATASET_REF, BIGQUERY_CLIENT, DATASET_ID, TEST_TABLE_ID
 def insert_gbq_data():
     table_ref = DATASET_REF.table(TEST_TABLE_ID)
     job_config = bigquery.LoadJobConfig()
-    job_config.source_format = bigquery.SourceFormat.CSV
+    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
     job_config.autodetect = True
-    job_config.write_disposition = bigquery.WriteDisposition.WRITE_EMPTY
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
 
-    with open('assets/test_table_assets.csv', 'rb') as source_file:
+    with open('assets/test_table_assets.json', 'rb') as source_file:
         job = BIGQUERY_CLIENT.load_table_from_file(
             source_file,
             table_ref,
