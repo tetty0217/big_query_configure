cf8bb3572e76c7497fa4604cbea8f441c623f654
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2019-01-23 18:16:38 +0900

    modified two partern for import data

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # big_query_configure
+## 1. python setting on local
+```
+pyenv install 2.7.13
+pyenv local 2.7.13
+python --version
+> 2.7.13
+
+pip install virtualenv
+virtualenv env
+. env/bin/activate
+pip install -r requirements.txt
+```
+
+## 2. create service key for GCP project
+https://cloud.google.com/bigquery/docs/reference/libraries?hl=ja
+
+## 3. global variable setting command insert shell
+```
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/your_host/.../your_google_credential.json"
+```
+
+## 4. 
--- insert_data.py
+++ insert_data.py
@@ -6,11 +6,21 @@ from gbq_setting import DATASET_REF, BIGQUERY_CLIENT, DATASET_ID, TEST_TABLE_ID
 def insert_gbq_data():
     table_ref = DATASET_REF.table(TEST_TABLE_ID)
     job_config = bigquery.LoadJobConfig()
-    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
     job_config.autodetect = True
+
+    # for CSV
+    job_config.source_format = bigquery.SourceFormat.CSV
+    job_config.skip_leading_rows = 1
+    source_path = 'assets/test_table_assets.csv'
+
+    # for JSON
+    # job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+    # source_path = 'assets/test_table_assets.json'
+
+    # WRITE_APPEND, WRITE_TRUNCATE, WRITE_EMPTY
     job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
 
-    with open('assets/test_table_assets.json', 'rb') as source_file:
+    with open(source_path, 'rb') as source_file:
         job = BIGQUERY_CLIENT.load_table_from_file(
             source_file,
             table_ref,
